2a7e981025dcb216ffee565ad8eeeedc351a7d06
Swikar Gyawali <swikargyawali710@gmail.com>
2026-07-25 21:14:26 +0545

    Checking the open source LLM

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from dotenv import load_dotenv 
+from langchain_mistralai import ChatMistralAI
 
 load_dotenv()
+
+model= ChatMistralAI (model="mistral-small-2603")
+
+result= model.invoke("Helloo")
+print(result.content)
